f35c30d533258dbd7d2afd959550f57cadd42475
Anurag <appylohar@gmail.com>
2025-10-25 16:53:12 +0530

    feat(terraform): minor update

--- ak-private-gke-cluster/main.tf
+++ ak-private-gke-cluster/main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "7.8.0"
-    }
-  }
-}
-
-# Configure the Google Cloud provider
-provider "google" {
-  project = "ak-here"
-  region  = "asia-south2"
-  zone    = "asia-south2-a"
-  credentials = "ak-here-key.json"
-}
-
 # VPC Network
 resource "google_compute_network" "ak_vpc" {
   name                    = "ak-vpc"
